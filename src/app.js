b077b073c395b434bd7e162e44fa2fbee63ea915
gustavopresende <gustavopresende@hotmail.com>
2022-07-07 00:37:41 -0300

    add files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import db from "./config/dbConnect.js";
 import router from "./routes/index.js";
 
+
+db.on('error',console.log.bind(console, 'erro de conexão'));
+db.once('open',()=>{
+    console.log('conexão feita com sucesso')
+})
 
 const app=express();
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ const router = (app)=>{
         }
         
     })
+
+    
 }
 
 
